9a30089abe8b3a1a38a7577bdcce3d2cbdf3ad04
Sérgio Oliveira <stoj97@gmail.com>
2018-03-04 22:29:49 +0000

    changing template

---
+++ GuguDadah/GuguDadah/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using GuguDadah.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuguDada.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title="GuguDada - de pais para pais";
+return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+    }
+}
