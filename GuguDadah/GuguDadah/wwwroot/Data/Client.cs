cf710e38fdbe3607906f59aee2098f9905332911
Sérgio Oliveira <stoj97@gmail.com>
2018-04-12 14:52:33 +0100

    updating interface

---
+++ GuguDadah/GuguDadah/wwwroot/Data/Client.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuguDadah.Data {
+
+    public class Client {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+
+        public string userName { get; set; }
+
+        public string password { get; set; }
+
+        public string eMail { get; set; }
+
+        public string contact { get; set; }
+
+        public byte[] avatar { get; set; }
+
+    }
+}
